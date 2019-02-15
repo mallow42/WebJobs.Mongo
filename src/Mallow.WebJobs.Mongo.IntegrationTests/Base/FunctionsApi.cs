f06bc464b198a6b27c5460cc82f7f062b10481f0
Mallow42 <mallow42@outlook.com>
2019-02-15 19:14:42 +0100

    Create HttpResponseMessageExtensions to allow fluent method chaining in FunctionsApi

--- src/Mallow.WebJobs.Mongo.IntegrationTests/Base/FunctionsApi.cs
+++ src/Mallow.WebJobs.Mongo.IntegrationTests/Base/FunctionsApi.cs
@@ -1,9 +1,7 @@
-﻿using System.Net.Http;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Flurl;
 using Flurl.Http;
 using MongoDB.Bson;
-using Newtonsoft.Json.Linq;
 
 namespace Mallow.WebJobs.Mongo.IntegrationTests.Base
 {
@@ -11,47 +9,33 @@ namespace Mallow.WebJobs.Mongo.IntegrationTests.Base
     {
         private const string BASE_URL = "http://localhost:7071/api";
 
-        public async Task<RequestResult> GetDocumentByObjectId(string collectionName, ObjectId documentId)
+        public Task<RequestResult> GetDocumentByObjectId(string collectionName, ObjectId documentId)
         {
-            var response = await BASE_URL.AppendPathSegment("GetDocumentByObjectId")
-                .SetQueryParam("collection", collectionName)
-                .SetQueryParam("id", documentId.ToString())
-                .AllowAnyHttpStatus()
-                .GetAsync();
-
-            return await CreateRequestResult(response);
+            return BASE_URL.AppendPathSegment("GetDocumentByObjectId")
+                           .SetQueryParam("collection", collectionName)
+                           .SetQueryParam("id", documentId.ToString())
+                           .AllowAnyHttpStatus()
+                           .GetAsync()
+                           .CreateRequestResult();
         }
 
-        public async Task<RequestResult> GetDocumentByStringId(string collectionName, string documentId)
+        public Task<RequestResult> GetDocumentByStringId(string collectionName, string documentId)
         {
-            var response = await BASE_URL.AppendPathSegment("GetDocumentByStringId")
-                .SetQueryParam("collection", collectionName)
-                .SetQueryParam("id", documentId)
-                .AllowAnyHttpStatus()
-                .GetAsync();
-            
-            return await CreateRequestResult(response);
+            return BASE_URL.AppendPathSegment("GetDocumentByStringId")
+                           .SetQueryParam("collection", collectionName)
+                           .SetQueryParam("id", documentId)
+                           .AllowAnyHttpStatus()
+                           .GetAsync()
+                           .CreateRequestResult();
         }
 
-        public async Task<RequestResult> InsertNewDocuments(string collectionName, object data)
+        public Task<RequestResult> InsertNewDocuments(string collectionName, object data)
         {
-            var response = await BASE_URL.AppendPathSegment("InsertNewDocuments")
-                .SetQueryParam("collection", collectionName)
-                .AllowAnyHttpStatus()
-                .PostJsonAsync(data);
-            
-            return await CreateRequestResult(response);
-        }
-        
-        private static async Task<RequestResult> CreateRequestResult(HttpResponseMessage result)
-        {
-            var content = await result.Content.ReadAsStringAsync();
-            if (result.IsSuccessStatusCode)
-            {
-                return new RequestResult(JObject.Parse(content), result.StatusCode);
-            }
-
-            return new RequestResult(content, result.StatusCode);
+            return BASE_URL.AppendPathSegment("InsertNewDocuments")
+                           .SetQueryParam("collection", collectionName)
+                           .AllowAnyHttpStatus()
+                           .PostJsonAsync(data)
+                           .CreateRequestResult();
         }
     }
 }
