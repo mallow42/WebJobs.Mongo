885cb99b48887025ca90c222b937e780052b3e8b
mallow42 <mallow42@outlook.com>
2019-04-14 14:45:11 +0200

    Use TestDocument instead of private class

--- src/Mallow.WebJobs.Mongo.UnitTests/Collector/MongoCreateCollectorTests.cs
+++ src/Mallow.WebJobs.Mongo.UnitTests/Collector/MongoCreateCollectorTests.cs
@@ -2,6 +2,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Mallow.Azure.WebJobs.Extensions.Mongo.Collector;
+using Mallow.WebJobs.Mongo.UnitTests.Converters;
 using Mallow.WebJobs.Mongo.UnitTests.Fakes;
 using MongoDB.Bson;
 using Xunit;
@@ -10,19 +11,19 @@ namespace Mallow.WebJobs.Mongo.UnitTests.Collector
 {
     public class MongoCreateCollectorTests
     {
-        private readonly MongoCreateCollector<TestDocument> _collector;
+        private readonly MongoCreateCollector<TestDocumentWithId<int>> _collector;
         private readonly CollectionFake _mongoCollection;
 
         public MongoCreateCollectorTests()
         {
             _mongoCollection = new CollectionFake();
-            _collector = new MongoCreateCollector<TestDocument>(_mongoCollection);
+            _collector = new MongoCreateCollector<TestDocumentWithId<int>>(_mongoCollection);
         }
 
         [Fact]
         public async Task AddAsync_DoesntAddAnyDocument()
         {
-            await _collector.AddAsync(new TestDocument("A", 1), CancellationToken.None);
+            await _collector.AddAsync(new TestDocumentWithId<int>("A", 1), CancellationToken.None);
 
             _mongoCollection.Documents.Should().BeEmpty();
         }
@@ -30,8 +31,8 @@ namespace Mallow.WebJobs.Mongo.UnitTests.Collector
         [Fact]
         public async Task FlushAsync_InsertsAllDocumentsInBatch()
         {
-            await _collector.AddAsync(new TestDocument("A", 1), CancellationToken.None);
-            await _collector.AddAsync(new TestDocument("B", 2), CancellationToken.None);
+            await _collector.AddAsync(new TestDocumentWithId<int>("A", 1), CancellationToken.None);
+            await _collector.AddAsync(new TestDocumentWithId<int>("B", 2), CancellationToken.None);
 
             await _collector.FlushAsync(CancellationToken.None);
 
@@ -46,22 +47,6 @@ namespace Mallow.WebJobs.Mongo.UnitTests.Collector
                 {"_id", 2}
             };
             _mongoCollection.Documents.Should().BeEquivalentTo(docA, docB);
-        }
-        
-        // ReSharper disable MemberCanBePrivate.Local
-        // ReSharper disable UnusedAutoPropertyAccessor.Local
-        private class TestDocument
-        {
-            
-            public string Name { get; }
-            
-            public int Id { get; }
-            
-            public TestDocument(string name, int id)
-            {
-                Name = name;
-                Id = id;
-            }
         }
     }
 }
